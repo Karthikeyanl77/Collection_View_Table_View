e69f1fede57fe20d094f6cbc222508e0152b749d
Karthikeyanl77 <karthisachin50@gmail.com>
2024-08-05 10:49:02 +0530

    Initial Commit

---
+++ Collection_View_Table_View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collection_View_Table_View
+//
+//  Created by Karthikeyan.L on 05/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
